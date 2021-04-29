c2de9df4d474464c0e84c198e8674eb9752f1ab5
Martin Luaces <martinluaces17@gmail.com>
2021-04-29 12:14:24 +0000

    semaforo

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
+	// const [variable,funcion(que modifica mi cariable)] = const ()
+	const [color, setColor] = useState("");
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<div id="barrita"></div>
+			<div id="semaforo">
+				<button
+					className={color == "red" ? color : ""}
+					onClick={() => setColor("red")}>
+					Red
+				</button>
+				<button
+					className={color == "yellow" ? color : ""}
+					onClick={() => setColor("yellow")}>
+					Yellow
+				</button>
+				<button
+					className={color == "green" ? color : ""}
+					onClick={() => setColor("green")}>
+					Green
+				</button>
+			</div>
 		</div>
 	);
 }
